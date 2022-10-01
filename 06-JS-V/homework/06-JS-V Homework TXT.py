422f28a97891c765714b463a52cdd8145539d025
Diego Cusmai <112570306+dcusmai@users.noreply.github.com>
2022-10-01 12:13:00 -0300

    Finished homework 06-JS-V

---
+++ 06-JS-V/homework/06-JS-V Homework TXT.py
@@ -0,0 +1,7 @@
+En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+prototype:
+Es una forma que tienen las Clases (Class) de permitir que cada objeto dentro de esa clase, pueda acceder a funciones (métodos) y propiedades, sin tener que estar repitiendo una y otra vez nuestro código.
+
+Constructors (de Clases):
+Para crear un objeto dentro de una cladse sin no repetir las propiedades de los objetos una y otra vez, y evitar errores si las propiedades cambian, podemos usar un constructor, donde establecemos las propiedades que tendrán todos los onjetos para esa clase.
