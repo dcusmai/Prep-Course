18e03ff27ea30bc3f8d66e5ebc4c3a7d6b5d4321
Diego Cusmai <112570306+dcusmai@users.noreply.github.com>
2022-10-01 21:48:02 -0300

    Avances homework 07-JS-VI

--- 06-JS-V/homework/06-JS-V Homework TXT.py
+++ 06-JS-V/homework/06-JS-V Homework TXT.py
@@ -5,3 +5,4 @@ Es una forma que tienen las Clases (Class) de permitir que cada objeto dentro de
 
 Constructors (de Clases):
 Para crear un objeto dentro de una cladse sin no repetir las propiedades de los objetos una y otra vez, y evitar errores si las propiedades cambian, podemos usar un constructor, donde establecemos las propiedades que tendrán todos los onjetos para esa clase.
+
